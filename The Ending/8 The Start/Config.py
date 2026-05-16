b72d7ee544144e5fbad5164073af3c415976c018
Toker <3374284195@qq.com>
2026-05-16 12:06:44 +0800

    添加配置文件

---
+++ The Ending/8 The Start/Config.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+import mypy
+
+@dataclass
+class WorldConfig():
+    size:int = 8
+    initial_food:int = 8
+
+@dataclass
+class OrganismConfig():
+    energy:int = 100
+    food_energy:int = 30
+    energy_to_reproduct:int = 150
